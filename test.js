d2d281a44dbae2c8f67d32e43d0c7ba5e68c81d8
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2013-05-29 05:26:32 +0200

    paced datamatrix, filled with options

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
 var http = require('http');
-var BarcodeDatamatrix = require('./lib/datamatrix').BarcodeDatamatrix;
 var label = require('./lib/label');
 
-var datamatrix = new BarcodeDatamatrix();
-var dm = datamatrix.getDigit('H',false);
-		
+
 
 var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-	 var svg = label.svg();
-	 res.end('<html><body>'+svg+'</body></html>');
+	res.writeHead(200);
+	var svg = label.Label({});
+	res.end('<html><body>'+svg+'</body></html>');
 });
 
 server.listen(parseInt(process.env.PORT) || 3000);
